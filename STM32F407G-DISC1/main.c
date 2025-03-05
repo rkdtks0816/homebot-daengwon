cf13b8b450cca8f297dd8f575c6736f1600116cb
rkdtks0816 <rkdtks0816@naver.com>
2025-03-05 20:53:24 +0900

    feat: 기본 폴더 구조 정리리

--- STM32F407G-DISC1/main.c
+++
@@ -1,9 +0,0 @@
-#include "stm32f4xx.h"
-
-int main(void) {
-    GPIOD->MODER |= (1 << (12 * 2));
-    while (1) {
-        GPIOD->ODR ^= (1 << 12);
-        for (volatile int i = 0; i < 1000000; i++);
-    }
-}
